24f1913ba8434186cf52bcff75b8539e70f81370
Wael Hasan <wael.hsn@outlook.com>
2020-12-18 02:35:37 +0100

    Adjust methdos and code cleanup. Fixing some bugs

--- src/MonsterTruckDrivingTest/DimensionsPositionsCrator.cs
+++ src/MonsterTruckDrivingTest/DimensionsPositionsCrator.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System;
+using System.Linq;
+using static System.Console;
 
 namespace MonsterTruckDrivingTest.Model
 {
@@ -6,13 +8,18 @@ namespace MonsterTruckDrivingTest.Model
     {
         public int width =  new SurfaceProperties().Width;
         public int length =  new SurfaceProperties().Length;
+        public string commands = new VehicleProperties().Commands;
+        public bool pass = new VehicleProperties().Pass;
         public int X = new Position().X;
         public int Y = new Position().Y;
+        public int currentX = new Position().CurrentX;
+        public int currentY = new Position().CurrentY;
+        public Directions initialDirection;
 
         public void CreateNewDimensions()
         {
-            var vehicle = new VehicleProperties();
             Write($"Width and length of the surface (in terms of Width, Length (e.g: 10,10)): ");
+
             var dimensions = ReadLine();
             try
             {
@@ -26,15 +33,13 @@ namespace MonsterTruckDrivingTest.Model
                 int.TryParse(dimensions.Split(',')[0], out _) &&
                 int.TryParse(dimensions.Split(',')[1], out _) &&
                 width > 0 && length > 0)
-                vehicle.Pass = true;
+                pass = true;
             else
                 WriteLine("ERROR. Invalid dimensions. Please try again.");
         }
 
         public void CreateNewPosition()
         {
-            var vehicle = new VehicleProperties();
-            var vehiclePosition = new Position();
             Write($"Starting position of the monstertruck (in terms of X and Y (e.g: 0,0)): ");
             var position = ReadLine();
             try
@@ -48,11 +53,96 @@ namespace MonsterTruckDrivingTest.Model
             if (position.Contains(',') &&
                 int.TryParse(position.Split(',')[0], out _) &&
                 int.TryParse(position.Split(',')[1], out _) &&
-                vehiclePosition.X >= 0 && vehiclePosition.Y >= 0 &&
-                vehiclePosition.X <= width - 1 && vehiclePosition.Y <= width - 1)
-                vehicle.Pass = true;
+                X >= 0 && Y >= 0 &&
+                X <= width - 1 && Y <= width - 1)
+                pass = true;
             else
                 WriteLine("ERROR. Invalid coordinators. Please try again.");
+        }
+
+        public void DirectionsValidation()
+        {
+            do
+            {
+                Write($"Direction of the monstertruck at the start point (North, East, South or West): ");
+                bool isValid = Enum.TryParse(ReadLine(), out initialDirection);
+                if (isValid)
+                    pass = true;
+                else
+                    WriteLine("ERROR. Invalid direction. Please try again.");
+            } while (!pass);
+
+            WriteLine(@"
+                      The following commands are supported for execution:
+                       * F = Forwards one step.
+                       * B = Backwards one step.
+                       * R = Rotate 90° to the right.
+                       * L = Rotate 90° to the left.
+                        ");
+
+            //Setting the allowed commands for this driving session.
+            do
+            {
+                var allowedCommands = new[] { 'F', 'B', 'R', 'L' };
+                Write("Commands to be executed for final driving result: ");
+                commands = ReadLine().ToUpper();
+
+                foreach (var command in commands)
+
+
+                    if (!allowedCommands.Contains(command))
+                        pass = false;
+                    if (!pass)
+                        WriteLine("ERROR: Commands contain a character that doesn't belong to allowed set. Please try again.");
+
+            } while (!pass);
+        }
+
+        public void GearStick()
+        {
+            currentX = X;
+            currentY = Y;
+            Directions currentDirection = initialDirection;
+
+            foreach (var command in commands)
+            {
+                //Commands switch of possibilities scenarios.
+                switch (command)
+                {
+                    //Steps cases
+                    case 'F':
+                        Write("Forwarded ");
+                        currentX += currentDirection == Directions.East ? 1 : currentDirection == Directions.West ? -1 : 0;
+                        currentY += currentDirection == Directions.North ? 1 : currentDirection == Directions.South ? -1 : 0;
+                        break;
+
+                    case 'B':
+                        Write("Backwarded ");
+                        currentX += currentDirection == Directions.East ? -1 : currentDirection == Directions.West ? 1 : 0;
+                        currentY += currentDirection == Directions.North ? -1 : currentDirection == Directions.South ? 1 : 0;
+                        break;
+
+                    //Rotation cases
+                    case 'R':
+                        Write("Rotating 90° to the right ");
+                        currentDirection = (currentDirection + 1);
+                        break;
+
+                    case 'L':
+                        Write("Rotating 90° to the left, ");
+                        currentDirection = currentDirection == 0 ? Directions.West : currentDirection - 1;
+                        break;
+                }
+
+                //checker, if we hit a wall.
+                if (currentX < 0 || currentY < 0 || currentX >= width || currentY >= length)
+                {
+                    WriteLine($"Uh oh, we hit the wall!({currentX}, {currentY})");
+                    break;
+                }
+
+                WriteLine($"{currentX}, {currentY} as a postion, \nDirection: {currentDirection}");
+            }
         }
     }
 }
--- src/MonsterTruckDrivingTest/Program.cs
+++ src/MonsterTruckDrivingTest/Program.cs
@@ -1,6 +1,4 @@
 ﻿using MonsterTruckDrivingTest.Model;
-using System;
-using System.Linq;
 using static System.Console;
 
 namespace MonsterTruckDrivingTest
@@ -12,110 +10,34 @@ namespace MonsterTruckDrivingTest
             while (true)
             {
                 WriteLine("* * * Welcome to Monster Truck driving test * * *");
-                var vehicle = new VehicleProperties();
-                var vehiclePosition = new Position();
+              //  var gearBox = new GearBox();
                 var dimensionsPositionsCrator = new DimensionsPositionsCrator();
 
                 //Input and validation of surface width and length.
                 do
                 {
                     dimensionsPositionsCrator.CreateNewDimensions();
-                    break;
-                } while (!vehicle.Pass);
+                } while (!dimensionsPositionsCrator.pass);
 
                 //Input and validation of x, y positions.
-                vehicle.Pass = false;
+                dimensionsPositionsCrator.pass = false;
                 do
                 {
                     dimensionsPositionsCrator.CreateNewPosition();
-                    break;
-                } while (!vehicle.Pass);
+                } while (!dimensionsPositionsCrator.pass);
 
                 //Input and validation of direction.
-                vehicle.Pass = false;
+                dimensionsPositionsCrator.pass = false;
 
-                Directions initialDirection;
+               // Directions initialDirection;
                 do
                 {
-                    Write($"Direction of the monstertruck at the start point (North, East, South or West): ");
-                    bool isValid = Enum.TryParse(ReadLine(), out initialDirection);
-                    if (isValid)
-                        vehicle.Pass = true;
-                    else
-                        WriteLine("ERROR. Invalid direction. Please try again.");
-                } while (!vehicle.Pass);
+                    dimensionsPositionsCrator.DirectionsValidation();
+                } while (!dimensionsPositionsCrator.pass);
 
-                WriteLine(@"
-                      The following commands are supported for execution:
-                       * F = Forwards one step.
-                       * B = Backwards one step.
-                       * R = Rotate 90° to the right.
-                       * L = Rotate 90° to the left.
-                        ");
-
-                // Input and validation of driving commands.
-                do
-                {
-                    //Setting the allowed commands for this driving session.
-                    var allowedCommands = new[] { 'F', 'B', 'R', 'L' };
-                    Write("Commands to be executed for final driving result: ");
-                    vehicle.Commands = ReadLine().ToUpper();
-
-                    foreach (var command in vehicle.Commands)
-
-                        if (!allowedCommands.Contains(command))
-                            vehicle.Pass = false;
-
-                    if (!vehicle.Pass)
-                        WriteLine("ERROR: Commands contain a character that doesn't belong to allowed set. Please try again.");
-
-                } while (!vehicle.Pass);
-
-                vehiclePosition.CurrentX = dimensionsPositionsCrator.X;
-                vehiclePosition.CurrentY = dimensionsPositionsCrator.Y;
-
-                Directions currentDirection = initialDirection;
-                //Process the commands.
-                foreach (var command in vehicle.Commands)
-                {
-                    //Commands switch of possibilities scenarios.
-                    switch (command)
-                    {
-                        //Steps cases
-                        case 'F':
-                            Write("Forwarded ");
-                            vehiclePosition.CurrentX += currentDirection == Directions.East ? 1 : currentDirection == Directions.West ? -1 : 0;
-                            vehiclePosition.CurrentY += currentDirection == Directions.North ? 1 : currentDirection == Directions.South ? -1 : 0;
-                            break;
-
-                        case 'B':
-                            Write("Backwarded ");
-                            vehiclePosition.CurrentX += currentDirection == Directions.East ? -1 : currentDirection == Directions.West ? 1 : 0;
-                            vehiclePosition.CurrentY += currentDirection == Directions.North ? -1 : currentDirection == Directions.South ? 1 : 0;
-                            break;
-
-                        //Rotation cases
-                        case 'R':
-                            Write("Rotating 90° to the right ");
-                            currentDirection = (currentDirection + 1);
-                            break;
-
-                        case 'L':
-                            Write("Rotating 90° to the left, ");
-                            currentDirection = currentDirection == 0 ? Directions.West : currentDirection - 1;
-                            break;
-                    }
-
-                    //checker, if we hit a wall.
-                    if (vehiclePosition.CurrentX < 0 || vehiclePosition.CurrentY < 0 || vehiclePosition.CurrentX >= dimensionsPositionsCrator.width || vehiclePosition.CurrentY >= dimensionsPositionsCrator.length)
-                    {
-                        WriteLine($"Uh oh, we hit the wall!({vehiclePosition.CurrentX}, {vehiclePosition.CurrentY})");
-                        break;
-                    }
-
-                    WriteLine($"{vehiclePosition.CurrentX}, {vehiclePosition.CurrentY} as a postion, \nDirection: {currentDirection}");
-                }
-
+                // the commands.
+                dimensionsPositionsCrator.GearStick();
+                
             }
         }
     }
